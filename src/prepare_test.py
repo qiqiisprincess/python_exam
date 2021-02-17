dc2f103a4752c9ca709e638d9797e9c56a442c0d
qiqiisprincess <deng97413@icloud.com>
2021-02-17 19:49:52 +0300

    Fix test files

--- src/prepare_test.py
+++ src/prepare_test.py
@@ -31,28 +31,28 @@ def remove_dir_r(path):
 
 if __name__ == '__main__':
     print(os.getcwd())
-    remove_dir_r('../test')
+    remove_dir_r('test')
     header = 'Names Seq'
 
     # test check_file
-    create_file('../test/checkfile/case1_ok.txt', [header, 'A a B b C c CTAG'])
-    create_file('../test/checkfile/case2_f.txt', '')
-    create_file('../test/checkfile/case3_f.txt', [header, 'A a B b C c CTAGDBI'])
-    create_file('../test/checkfile/case4_f.txt', ['Header Seqth', 'A a B b C c CTAGTT'])
-    create_file('../test/checkfile/case5_ok.txt',
+    create_file('test/checkfile/case1_ok.txt', [header, 'A a B b C c CTAG'])
+    create_file('test/checkfile/case2_f.txt', '')
+    create_file('test/checkfile/case3_f.txt', [header, 'A a B b C c CTAGDBI'])
+    create_file('test/checkfile/case4_f.txt', ['Header Seqth', 'A a B b C c CTAGTT'])
+    create_file('test/checkfile/case5_ok.txt',
                 [
                     header,
                     'A a B b C c CTCCTAG',
                     'C c D d G g CTTAGG'
                 ])
-    create_file('../test/checkfile/case6_f.rtf', [header, 'A a B b C CTAG'])
+    create_file('test/checkfile/case6_f.rtf', [header, 'A a B b C CTAG'])
 
     # test science_graph
-    create_file('../test/scigraph/case1/1.txt', [header, 'A a B b C c CTAG'])
+    create_file('test/scigraph/case1/1.txt', [header, 'A a B b C c CTAG'])
 
-    create_file('../test/scigraph/case2/1.txt', [header, 'A a B b C c CCTAGG'])
-    create_file('../test/scigraph/case2/2.txt', [header, 'D a E b G c CCTAACTGG'])
+    create_file('test/scigraph/case2/1.txt', [header, 'A a B b C c CCTAGG'])
+    create_file('test/scigraph/case2/2.txt', [header, 'D a E b G c CCTAACTGG'])
 
-    create_file('../test/scigraph/case3/1.txt', [header, 'A a B b C c CCTAACTGG'])
-    create_file('../test/scigraph/case3/2.txt', [header, 'A a E e G g CCTAATTGG'])
-    create_file('../test/scigraph/case3/3.txt', [header, 'D d F f H h CCTAACTGG'])
+    create_file('test/scigraph/case3/1.txt', [header, 'A a B b C c CCTAACTGG'])
+    create_file('test/scigraph/case3/2.txt', [header, 'A a E e G g CCTAATTGG'])
+    create_file('test/scigraph/case3/3.txt', [header, 'D d F f H h CCTAACTGG'])
